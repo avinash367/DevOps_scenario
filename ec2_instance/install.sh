c6c669bd671d1363af9f2fd9a8f74dbad52b8cc8
Avinash <meesalasaiavinash@gmail.com>
2021-03-25 21:43:19 +0100

    Adding jenkins_vault_sonarqube usnig terraform

---
+++ ec2_instance/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install docker
+apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+usermod -aG docker ubuntu
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
